683c994ac7303663cd7a70b79ce08ef8fb612ae8
Muhammad Hasham Khalid <m.hasham681@gmail.com>
2020-05-25 12:55:07 +0500

    Initial Commit 📄📄

---
+++ js/main.js
@@ -0,0 +1,93 @@
+const url = '../docs/pdf1.pdf';
+
+let pdfDoc = null,
+    pageNum = 1,
+    pageIsRendering = false,
+    pageNumIsPending = null;
+
+const scale = 1.5,
+    canvas = document.querySelector("#pdf-render"),
+    ctx = canvas.getContext('2d');
+
+// Render the page
+const renderPage = num => {
+    pageIsRendering = true;
+
+    // Get page
+    pdfDoc.getPage(num).then(page => {
+        // Set scale
+        const viewport = page.getViewport({
+            scale
+        });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderCtx = {
+            canvasContext: ctx,
+            viewport
+        }
+
+        page.render(renderCtx).promise.then(() => {
+            pageIsRendering = false;
+
+            if (pageNumIsPending !== null) {
+                renderPage(pageNumIsPending);
+                pageNumIsPending = null;
+            }
+        });
+
+        // Output current page
+        document.querySelector("#page-num").textContent = num;
+    });
+};
+
+// Check for pages rendering
+const queueRenderPage = num => {
+    if (pageIsRendering) {
+        pageNumIsPending = num;
+    } else {
+        renderPage(num);
+    }
+};
+
+// Show previous page
+const showPrevPage = () => {
+    if (pageNum <= 1) {
+        return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+}
+
+// Show next page
+const showNextPage = () => {
+    if (pageNum >= pdfDoc.numPages) {
+        return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
+}
+
+// Get the document
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+        pdfDoc = pdfDoc_;
+        document.querySelector("#page-count").textContent = pdfDoc.numPages;
+        // console.log(pdfDoc);
+
+        renderPage(pageNum);
+    })
+    .catch(err => {
+        // Display Error
+        const div = document.createElement('div');
+        div.className = 'error';
+        div.appendChild(document.createTextNode(err.message));
+        document.querySelector('body').insertBefore(div, canvas);
+
+        // remove top-bar
+        document.querySelector('.top-bar').style.display = 'none';
+    });
+
+// Button Events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+
+document.querySelector('#next-page').addEventListener('click', showNextPage);
